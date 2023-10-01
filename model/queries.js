94863b7cf7a73659eb09393db440a34b9a724896
Nikolaus Albert <115626969+BiruSkai@users.noreply.github.com>
2023-10-01 21:38:50 +0700

    added completed queries.js for registerUser()

--- model/queries.js
+++ model/queries.js
@@ -7,6 +7,17 @@ class Queries {
         }
 
         async registerUser() {
-                
+                const {title, fullname, hashedPass, gender, birth_date, email, telephone, user_type} = this.schema.userDetails;
+                try {
+                        const user = await pool.query(`
+                        INSERT INTO user_data(title, fullname, password, gender, birth_date, email, 
+                                telephone, user_type, created_at) VALUES('${title}','${fullname}','${hashedPass}','${gender}',
+                                '${birth_date}','${email}','${telephone}','${user_type}',now())
+                                `);
+                        const userId = await pool.query(`SELECT id FROM user_data WHERE email = '${email}'`);
+                        return {error: false, data: userId.rows[0]};
+                } catch(err) {
+                        return {error: true, errorMessage: 'A Problem occurred.Please try a different username or/and password.'}
+                };
         }
 }
